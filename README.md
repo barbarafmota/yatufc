296bd179dfc9b2c2b790323ff1f30c03cf89b60d
Lucas Lira Gomes <x8lucas8x@gmail.com>
2017-10-08 16:06:59 +0200

    Update yatufc to be able to crawl from twitter search.

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 
 ## **Introduction**
 
-This program can crawl a given Twitter User timeline and output his/her tweets in a CSV file for further analysis. Also, it is possible to set a date range for which you are interested, therefore capturing only tweets in the desired timeslot.
+This program can crawl a given Twitter user timeline or simply search for a provided term, in order to output found tweets in a CSV file for further analysis. Also, it is possible to set a date range for which you are interested, therefore capturing only tweets in the desired timeslot.
 
-**ATTENTION**: This method can only return up to 3,200 of a user's most recent Tweets, due to Twitter's API limitations. Also, be warned that Twitter imposes a requests per rate limit window of 180/user and 300/app, per timeslot of 15 minutes For more info about the API call in question, see [this](https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline).
+**ATTENTION**: Twitter API is rate limited. The rates per endpoint are described in [here](https://developer.twitter.com/en/docs/basics/rate-limits). If YATUFC hits one of those limits, it'll sleep for 15 minutes after when they're finally reset.
 
 ## **Authors**
 Read AUTHORS
@@ -18,5 +18,3 @@ Read LICENSE
  1. pip3 install -r /path/to/requirements.txt
  2. chmod +x yatufc
  3. ./yatufc ...
-
-
--- yatufc.py
+++ yatufc.py
@@ -5,126 +5,175 @@ import os
 import twython as tw
 import pandas as pd
 
+from time import sleep
 from datetime import datetime, date
 from argparse import ArgumentParser
 
-def create_date(string):
-    '''Return a date instance.'''
+DATE_FORMAT = '%a %b %d %H:%M:%S +0000 %Y'
+
+
+def _create_date(string):
     return datetime.strptime(string, '%Y-%m-%d').date()
 
-def create_twython(app_key, app_secret, oauth_token, oauth_token_secret):
-    '''Return a twython instance.'''
+
+def _create_twython(app_key, app_secret, oauth_token, oauth_token_secret):
     return tw.Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 
-def parse_arguments():
-    '''Return command line arguments.'''
+
+def _parse_arguments():
     parser = ArgumentParser(
         prog='YATUFC',
-        description='Yet Another Twitter User Timeline Crawler (YATUFC) can '
-        'crawl a given Twitter user timeline and output his/her tweets to a '
-        'CSV file for further analysis.',
-        epilog='ATTENTION: This program can only return up to 3,200 of a '
-        'user\'s most recent Tweets, due to Twitter\'s API limitations. '
-        'Also, be warned that Twitter imposes a requests per rate limit '
-        'window of 180/user and 300/app, per timeslot of 15 minutes For more '
-        'info about the API call in question, see '
-        'https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline.')
+        description='This program can crawl a given Twitter user timeline '
+        'or simply search for a provided term, in order to output found tweets '
+        'in a CSV file for further analysis. Also, it is possible to set a date '
+        'range for which you are interested, therefore capturing only tweets in '
+        'the desired timeslot.',
+        epilog='ATTENTION: Twitter API is rate limited. The rates per endpoint '
+        'are described in https://developer.twitter.com/en/docs/basics/rate-limits. '
+        'If YATUFC hits one of those limits, it\'ll sleep for 15 minutes after when '
+        'they\'re finally reset.')
     parser.add_argument(
-        '-s', '--since', type=create_date, required=False,
+        '-s', '--since', type=_create_date, required=False,
         default='0001-1-1', metavar='YEAR-MONTH-DAY',
         help='a date in the format YEAR-MONTH-DAY (default: no limit)')
     parser.add_argument(
-        '-u', '--upTo', type=create_date, required=False,
+        '-u', '--upTo', type=_create_date, required=False,
         default=date.today().isoformat(), metavar='YEAR-MONTH-DAY',
         help='a date in the format YEAR-MONTH-DAY (default: now)')
     parser.add_argument(
         '-o', '--outputFilePath', type=str, required=False,
         metavar='outputFilePath', default='output.csv',
         help='a path to save the output data (default: output.csv)')
-    parser.add_argument('profileName', metavar='the profile name to crawl.')
+
+    subparsers = parser.add_subparsers(help='sub-command help', dest='command')
+    parser_profile = subparsers.add_parser('profile')
+    parser_profile.add_argument('profileName', metavar='the profile name to crawl.')
+
+    parser_search = subparsers.add_parser('search')
+    parser_search.add_argument('searchTerm', metavar='the search term to crawl.')
+
     return parser.parse_args()
 
-def generate_tweets_table(twitter, profile_name, since, up_to):
+
+def _fetch_from_profile(twitter_session, term, max_id):
+    results = twitter_session.get_user_timeline(screen_name=term,
+                                                max_id=max_id,
+                                                count=200)
+
+    return results
+
+
+def _fetch_from_search(twitter_session, term, max_id):
+    results = twitter_session.search(q=term,
+                                     max_id=max_id,
+                                     count=200)
+
+    return results['statuses'] if 'statuses' in results else []
+
+
+def _generate_profile_dataframe(fetch_tweets, term, since, up_to):
     '''Return a dataframe filled with the crawled tweets.'''
-    tweets = pd.DataFrame(
-        columns=['creationDate', 'creationTime', 'profileName',
-                 'totalOfRetweets', 'totalOfFavourites', 'tweet'])
-    tweets.set_index(['creationDate', 'creationTime'], inplace=True)
+    tweets_df = pd.DataFrame(
+        columns=['creationDate', 'creationTime', 'profileName', 'profileFollowersCount',
+                 'profileFriendsCount', 'profileListsInCount', 'profileFavoritesCount',
+                 'profileTweetsCount', 'retweetsCount', 'favoritesCount', 'tweet'])
+    tweets_df.set_index(['creationDate', 'creationTime'], inplace=True)
 
     max_id = None
-    date_format = '%a %b %d %H:%M:%S +0000 %Y'
 
     while True:
         try:
-            results = twitter.get_user_timeline(screen_name=profile_name,
-                                                max_id=max_id,
-                                                count=200)
+            twitter_session = _get_twitter_session()
+            results = fetch_tweets(twitter_session, term, max_id)
         except tw.TwythonRateLimitError as ex:
             msg, *_ = ex.args
-            raise Exception('ERROR: ' + msg + '. Try again after 15 minutes.')
+            # Sleep for 15 minutes and try again, as recommended by the docs.
+            print('WARNING: Twitter API returned a 429 (Too Many Requests), Rate limit exceeded. '
+                  'This program will sleep for 15 minutes after when the limits are finally reset.')
+            sleep((15 * 60) + 1)
+            continue
         except (tw.TwythonAuthError, tw.TwythonError) as ex:
             msg, *_ = ex.args
-            raise Exception('ERROR: ' + msg + '.')
+            raise RuntimeError('ERROR: ' + msg + '.')
 
         if not results:
-            print('>>> Limit of 3200 tweets reached.')
+            print('>>> Either no more data available or time range/rate limits were hit.')
             break
 
         for tweet in results:
             created_date = datetime.strptime(tweet['created_at'],
-                                             date_format).date()
+                                             DATE_FORMAT).date()
             created_time = datetime.strptime(tweet['created_at'],
-                                             date_format).time()
+                                             DATE_FORMAT).time()
 
             if created_date < since:
-                return tweets
+                return tweets_df
 
             max_id = tweet['id']
 
             if created_date >= since and created_date <= up_to:
-                row = {'creationDate':created_date.isoformat(),
-                       'creationTime':created_time.isoformat(),
-                       'profileName':profile_name,
-                       'totalOfRetweets':tweet['retweet_count'],
-                       'totalOfFavourites':tweet['favorite_count'],
-                       'tweet':tweet['text']}
-                tweets = tweets.append(row, ignore_index=True)
+                row = {'creationDate': created_date.isoformat(),
+                       'creationTime': created_time.isoformat(),
+                       'profileName': tweet['user']['screen_name'],
+                       'profileFollowersCount': tweet['user']['followers_count'],
+                       'profileFriendsCount': tweet['user']['friends_count'],
+                       'profileListsInCount': tweet['user']['listed_count'],
+                       'profileFavoritesCount': tweet['user']['favourites_count'],
+                       'profileTweetsCount': tweet['user']['statuses_count'],
+                       'retweetsCount': tweet['retweet_count'],
+                       'favoritesCount': tweet['favorite_count'],
+                       'tweet': tweet['text']}
+                tweets_df = tweets_df.append(row, ignore_index=True)
 
         max_id -= 1
 
-    return tweets
+    return tweets_df
 
-def main():
-    '''Main function.'''
-    arguments = parse_arguments()
-
-    if arguments.since > arguments.upTo:
-        raise Exception('ERROR: You cannot set since\'s date after upTo.')
-    elif arguments.upTo > date.today():
-        raise Exception('ERROR: Thou shalt not crawl tweets from the future.')
-
+def _get_twitter_session():
     auth_files = ['app_key', 'app_secret', 'oauth_token', 'oauth_token_secret']
     credentials = {}
 
     for file_name in auth_files:
         if not os.path.isfile(file_name) or not os.access(file_name, os.R_OK):
-            raise Exception('Either file \"' + file_name + '\" is missing or '
-                            'is not readable. It is required for '
-                            'authentication, so please register your Twitter '
-                            'APP first and save the respective credential in a '
-                            'file at the same folder of this executable.')
+            raise IOError('Either file \"{}\" is missing or '
+                          'is not readable. It is required for '
+                          'authentication, so please register your Twitter '
+                          'APP first and save the respective credential in a '
+                          'file at the same folder of this executable.'.format(file_name))
         else:
             with open(file_name, 'r') as file:
                 credentials[file_name] = file.readline()[:-1]
 
-    twython_instance = create_twython(**credentials) # pylint: disable=star-args
+    return _create_twython(**credentials) # pylint: disable=star-args
+
+def main():
+    '''Main function.'''
+    arguments = _parse_arguments()
+
+    if arguments.since > arguments.upTo:
+        raise ValueError('ERROR: You cannot set since\'s date after upTo.')
+    elif arguments.upTo > date.today():
+        raise ValueError('ERROR: Thou shalt not crawl tweets from the future.')
 
     print('Yet Another Twitter User Timeline Crawler (YATUFC)')
     print('*** Started crawling!!!')
-    tweets = generate_tweets_table(twython_instance,
-                                   arguments.profileName,
-                                   arguments.since,
-                                   arguments.upTo)
+
+    fetch_tweets = None
+    term = None
+
+    if arguments.command == 'profile':
+        fetch_tweets = _fetch_from_profile
+        term = arguments.profileName
+    elif arguments.command == 'search':
+        fetch_tweets = _fetch_from_search
+        term = arguments.searchTerm
+    else:
+        raise ValueError('ERROR: Unknown sub-command.')
+
+    tweets = _generate_profile_dataframe(fetch_tweets,
+                                         term,
+                                         arguments.since,
+                                         arguments.upTo)
     print('*** Finished crawling!!!')
     print('*** Total of Tweets: ' + str(len(tweets)))
     print('*** Output was saved in ' + arguments.outputFilePath)
